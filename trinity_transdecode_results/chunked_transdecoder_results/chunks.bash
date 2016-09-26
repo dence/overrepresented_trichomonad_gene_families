4d11e583cdb237fb092059afeec0871063b9f998
Daniel Ence <dandence@gmail.com>
2016-09-26 14:38:29 -0600

    updated

---
+++ trinity_transdecode_results/chunked_transdecoder_results/chunks.bash
@@ -0,0 +1,19 @@
+mkdir 30235.Trinity.fixed_IDs.fasta.transdecoder; cd 30235.Trinity.fixed_IDs.fasta.transdecoder; fasta_tool --chunks 10 ../30235.Trinity.fixed_IDs.fasta.transdecoder.pep
+cd ..
+mkdir G3.Trinity.fixed_IDs.fasta.transdecoder; cd G3.Trinity.fixed_IDs.fasta.transdecoder; fasta_tool --chunks 10 ../G3.Trinity.fixed_IDs.fasta.transdecoder.pep
+cd ..
+mkdir NCBI.foetus.bovine.trinity.fasta.transdecoder; cd NCBI.foetus.bovine.trinity.fasta.transdecoder; fasta_tool --chunks 11 ../NCBI.foetus.bovine.trinity.fasta.transdecoder.pep
+cd ..
+mkdir NCBI.foetus.feline.trinity.fasta.transdecoder; cd NCBI.foetus.feline.trinity.fasta.transdecoder; fasta_tool --chunks 10 ../NCBI.foetus.feline.trinity.fasta.transdecoder.pep
+cd ..
+mkdir Pentatrich.homi.trinity.fasta.transdecoder; cd Pentatrich.homi.trinity.fasta.transdecoder; fasta_tool --chunks 22 ../Pentatrich.homi.trinity.fasta.transdecoder.pep
+cd ..
+mkdir t016.Trinity.fixed_IDs.fasta.transdecoder; cd t016.Trinity.fixed_IDs.fasta.transdecoder; fasta_tool --chunks 15 ../t016.Trinity.fixed_IDs.fasta.transdecoder.pep
+cd ..
+mkdir Tetratrich.gall.trinity.fasta.transdecoder; cd Tetratrich.gall.trinity.fasta.transdecoder; fasta_tool --chunks 16 ../Tetratrich.gall.trinity.fasta.transdecoder.pep
+cd ..
+mkdir Trich.gall.trinity.fasta.transdecoder; cd Trich.gall.trinity.fasta.transdecoder; fasta_tool --chunks 13 ../Trich.gall.trinity.fasta.transdecoder.pep
+cd ..
+mkdir Trichomitus.batr.trinity.fasta.transdecoder; cd Trichomitus.batr.trinity.fasta.transdecoder; fasta_tool --chunks 23 ../Trichomitus.batr.trinity.fasta.transdecoder.pep
+cd ..
+mkdir Trich.tenax.trinity.fasta.transdecoder; cd Trich.tenax.trinity.fasta.transdecoder; fasta_tool --chunks 15 ../Trich.tenax.trinity.fasta.transdecoder.pep
